b6791968fb181dcdb17b9942104fa6c6030a781a
Diana <lici4qweru090@gmail.com>
2022-12-03 11:11:15 +0500

    from int to character

--- Program.cs
+++ Program.cs
@@ -8,9 +8,38 @@ namespace Laba_5_Valeeva_PrI102
 {
     internal class Program
     {
+        static bool NumFloatOrNot(string num)
+        {
+            float a;
+            if (float.TryParse(num, out a))
+                return true;
+            return false;
+        }
+
+        static bool NumIntOrNot(string num)
+        {
+            int a;
+            if (int.TryParse(num, out a))
+                return true;
+            return false;
+        }
+
+        static void Task1()
+        {
+            Console.WriteLine("Введи число");
+            string strNum = Console.ReadLine();
+            int intNum;
+            float floatNum;
+            if (NumIntOrNot(strNum))
+            {
+                Console.WriteLine((char)Convert.ToInt32(strNum));
+            }
+
+        }
+
         static void Main(string[] args)
         {
-
+            Task1();
         }
     }
 }
