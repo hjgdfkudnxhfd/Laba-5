40c60fc6055032824173a74bd1cbcd510ccefbb8
Diana <lici4qweru090@gmail.com>
2022-12-03 11:54:54 +0500

    ADD are two float numbers same

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace Laba_5_Valeeva_PrI102
 {
@@ -26,17 +28,50 @@ namespace Laba_5_Valeeva_PrI102
 
         static void Task1()
         {
-            Console.WriteLine("Введи число");
-            string strNum = Console.ReadLine();
-            int intNum;
-            float floatNum;
-            if (NumIntOrNot(strNum))
+            
+            float floatNum1 = float.NaN;
+            float floatNum2;
+            while (true)
             {
-                Console.WriteLine((char)Convert.ToInt32(strNum));
+                Console.WriteLine("Задание 1. Для выхода нажмите 'q' ");
+                Console.WriteLine("Введи число: ");
+                string strNum = Console.ReadLine();
+                if (NumIntOrNot(strNum))
+                {
+                    Console.WriteLine((char)Convert.ToInt32(strNum));
+                    Console.ReadKey();
+                }
+                else if (NumFloatOrNot(strNum))
+                {
+                    floatNum2 = Convert.ToSingle(strNum);
+                    if (floatNum2 == floatNum1)
+                    {
+                        Console.WriteLine("Введеные числа равны, выход");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("Повторите ввод числа");
+                        floatNum1 = floatNum2;
+                        Thread.Sleep(1000);
+
+
+                    }
+                }
+                else if (strNum == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод, повторите попытку");
+                    Console.ReadKey();
+                }
+                Console.Clear();
             }
-
         }
-
+    
         static void Main(string[] args)
         {
             Task1();
